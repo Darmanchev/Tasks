b1c85232d6aee12ee159eba428184a26cfcd35c4
Darmanchev <95772945+Darmanchev@users.noreply.github.com>
2022-11-20 12:17:56 +0200

    Add files via upload

---
+++ week.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   // ___________________________Task 1
+   // int ap, ban, pear;
+   // cout << "Vvedite kolicestvo iablok,bananov i grush ";
+
+   // cin >> ap >> ban >> pear;
+
+   // cout << "Traicho, don't forget to buy " << ap << " apples, " << pear << " pears and " << ban << " bananas!";
+
+   // return 0;
+   // __________________________Task 2
+   // int FN, SN, Sum, Diff, Prod;
+
+   // cout << "Vvedite pervoe cislo i vtoroe cislo ";
+   // cin >> FN >> SN;
+
+   // Sum = FN + SN;
+   // Diff = FN - SN;
+   // Prod = FN * SN;
+
+   // cout << "Summ = " << Sum << "\n"
+   //      << "Difference = " << Diff << "\n"
+   //      << "Proisvedenie = " << Prod;
+
+   // return 0;
+   // ___________________-Task 3
+   // int Num, Second, Day, Hours, Minute;
+
+   // cout << "Input number ";
+   // cin >> Num;
+   // Day = Num / 86400;
+   // Hours = Num / 3600 % 24;
+   // Minute = Num / 60 % 60;
+   // Second = Num % 60;
+   // cout << Day << " day/s " << Hours << " hour/s " << Minute << " minute/s " << Second << " second/s ";
+   // return 0;
+
+   // ___________________Task 4
+
+   // int Num, Fir, Sec, Thr;
+
+   // cout << "Input number ";
+
+   // cin >> Num;
+
+   // Fir = Num / 100;
+   // Sec = Num / 10 % 10;
+   // Thr = Num % 10;
+
+   // cout << Thr << Sec << Fir;
+   // return 0;
+
+   // _____________________Task 5
+
+   // double h, d, P, apo, S, S1;
+   // double V;
+   // d = 4;
+   // apo = 6;
+   // h = 5;
+
+   // P = d * 4;
+
+   // S = d * d;
+
+   // V = (S*h)/3;
+   // S1 = P * apo / 2 + S;
+
+   // cout << "Arrea pira = " << S1 << " Volume = " << V;
+
+   // return 0;
+
+
+   int x1,y1,x2,y2,r1,r2;
+   cin>>x1>>y1>>x2>>y2>>r1>>r2;
+
+   int d1=x2-x1;
+   int d2=y2-y2;
+   double distance=sqrt(d1*d1+d2*d2);
+int r_sum=r1+r2;
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
